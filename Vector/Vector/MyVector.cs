9bfeda3d16a7ad79372a5493af6be6547cca21e7
Kevin Hetzendorfer <hetzi.hetzendorfer@gmx.at>
2018-03-07 14:43:50 +0100

    Finish of Vector

---
+++ Vector/Vector/MyVector.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vector
+{
+    struct MyVector
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Z { get; set; }
+        public int Dimension4 { get; set; }
+        public int Dimension5 { get; set; }
+        public int Dimension6 { get; set; }
+        public int Dimension7 { get; set; }
+        public int Dimension8 { get; set; }
+        public int Dimension9 { get; set; }
+        public int Dimension10 { get; set; }
+
+        public MyVector(int x, int y, int z, int a, int b, int c, int d, int e, int f, int g)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Z = z;
+            this.Dimension4 = a;
+            this.Dimension5 = b;
+            this.Dimension6 = c;
+            this.Dimension7 = d;
+            this.Dimension8 = e;
+            this.Dimension9 = f;
+            this.Dimension10 = g;
+        }
+
+        public static MyVector operator +(MyVector left, MyVector right)
+        {
+            MyVector vector = new MyVector();
+            vector.X = left.X + right.X;
+            vector.Y = left.Y + right.Y;
+            vector.Z = left.Z + right.Z;
+            vector.Dimension4 = left.Dimension4 + right.Dimension4;
+            vector.Dimension5 = left.Dimension5 + right.Dimension5;
+            vector.Dimension6 = left.Dimension6 + right.Dimension6;
+            vector.Dimension7 = left.Dimension7 + right.Dimension7;
+            vector.Dimension8 = left.Dimension8 + right.Dimension8;
+            vector.Dimension9 = left.Dimension9 + right.Dimension9;
+            vector.Dimension10 = left.Dimension10 + right.Dimension10;
+
+            return vector;
+        }
+        public static MyVector operator *(MyVector left, MyVector right)
+        {
+            MyVector vector = new MyVector();
+            vector.X = left.X * right.X;
+            vector.Y = left.Y * right.Y;
+            vector.Z = left.Z * right.Z;
+            vector.Dimension4 = left.Dimension4 * right.Dimension4;
+            vector.Dimension5 = left.Dimension5 * right.Dimension5;
+            vector.Dimension6 = left.Dimension6 * right.Dimension6;
+            vector.Dimension7 = left.Dimension7 * right.Dimension7;
+            vector.Dimension8 = left.Dimension8 * right.Dimension8;
+            vector.Dimension9 = left.Dimension9 * right.Dimension9;
+            vector.Dimension10 = left.Dimension10 * right.Dimension10;
+
+            return vector;
+        }
+    }
+}
--- Vector/Vector/Program.cs
+++ Vector/Vector/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace Vector
 {
@@ -10,6 +11,36 @@ namespace Vector
     {
         static void Main(string[] args)
         {
+            MyVector vec4 = new MyVector(1, 1, 1, 1, 1, 1, 1, 1, 1, 1);
+            MyVector vec2 = new MyVector(3, 3, 3, 3, 3, 3, 3, 3, 3, 3);
+
+            vec4 += vec2;
+
+
+            Stopwatch sw = new Stopwatch();
+            /*sw.Start();
+            MyVector vec1 = new MyVector(1, 1, 1, 1, 1, 1, 1, 1, 1, 1);
+            for (int i = 0; i < 1000000; i++)
+            {
+                MyVector vec3 = new MyVector(2, 2, 2, 2, 2, 2, 2, 2, 2, 2);
+                vec1 += vec3;
+                vec1 *= vec3;
+            }
+            sw.Stop(); */
+
+            sw.Start();
+            MyVectorClass vecClass1 = new MyVectorClass(1, 1, 1, 1, 1, 1, 1, 1, 1, 1);
+            for (int i = 0; i < 1000000; i++)
+            {
+                MyVectorClass vecClass3 = new MyVectorClass(2, 2, 2, 2, 2, 2, 2, 2, 2, 2);
+                vecClass1 += vecClass3;
+                vecClass1 *= vecClass3;
+            }
+            sw.Stop();
+
+            Console.WriteLine(sw.ElapsedMilliseconds);
+            Console.ReadLine();
+
         }
     }
 }
