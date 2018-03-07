1110b4f8ab33d9433634f4525a544f2c7f90c6ae
Kevin Hetzendorfer <hetzi.hetzendorfer@gmx.at>
2018-03-07 14:08:47 +0100

    Creation of Vector-Program

---
+++ Transaktion/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Transaktion;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Geld meinGeld = new Geld(100);
+            Geld deinGeld = new Geld(500);
+
+            Geld test = 500;
+            decimal val = (decimal)test;
+
+            meinGeld = meinGeld + deinGeld;
+            Console.WriteLine(meinGeld.Menge);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Transaktion/Transaktion/Geld.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transaktion
+{
+    public struct Geld
+    {
+        public decimal Menge { get; set; }
+
+        public Geld(decimal menge)
+        {
+            this.Menge = menge;
+        }
+
+        public static Geld operator +(Geld g1, Geld g2) => new Geld(g1.Menge + g2.Menge);
+
+        public static implicit operator Geld(decimal menge)
+        {
+            Geld geld = new Geld(menge);
+            return geld;
+        }        public static explicit operator decimal(Geld geld)
+        {
+            return geld.Menge;
+        }
+    }
+}
