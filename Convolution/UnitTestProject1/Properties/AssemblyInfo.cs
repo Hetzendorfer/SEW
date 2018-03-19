5ed185b08c4ec6cdf1e9214e91c19a172aad43ca
Kevin Hetzendorfer <hetzi.hetzendorfer@gmx.at>
2018-03-19 19:43:10 +0100

    Erstellung von Unit Tests

---
+++ Convolution/UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("eef86f25-74b3-4e30-9a05-db262e115c61")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Convolution/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Convolution;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMatrix()
+        {
+            SMatrix matrix1 = new SMatrix();
+            SMatrix matrix2 = new SMatrix();
+            int[,] arr1 = new int[8, 10];
+            int[,] arr2 = new int[3, 3];
+
+            Console.WriteLine("Matrix 1: ");
+            for (int y = 0; y < arr1.GetLength(1); y++)
+            {
+                for (int x = 0; x < arr1.GetLength(0); x++)
+                {
+                    arr1[x, y] = 3;
+                    Console.Write(arr1[x, y] + "| ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Matrix 2: ");
+            for (int y = 0; y < arr2.GetLength(1); y++)
+            {
+                for (int x = 0; x < arr2.GetLength(0); x++)
+                {
+                    arr2[x, y] = 2;
+                    Console.Write(arr2[x, y] + "| ");
+                }
+                Console.WriteLine();
+            }
+
+            matrix1.Dimension = arr1;
+            matrix2.Dimension = arr2;
+
+            SMatrix asd = matrix1 * matrix2;
+
+            for (int y = 0; y < asd.Dimension.GetLength(1); y++)
+            {
+                for (int x = 0; x < asd.Dimension.GetLength(0); x++)
+                {
+                    Console.Write(asd.Dimension[x, y] + "|");
+                }
+                Console.WriteLine();
+            }
+
+            Assert.AreEqual(asd.Dimension.GetLength(0), 6);
+            Assert.AreEqual(asd.Dimension.GetLength(1), 8);
+        }
+
+    
+    }
+}
