0eba22c15535fe11c0125eb96001faa3638b62e0
Kevin Hetzendorfer <hetzi.hetzendorfer@gmx.at>
2018-05-29 22:32:09 +0200

    asd

--- Xml/Xml/Program.cs
+++ Xml/Xml/Program.cs
@@ -12,6 +12,29 @@ namespace Xml
     {
         static void Main(string[] args)
         {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(XmlReader.Create("file.xml"));
+
+            XmlNodeList nodes = doc.SelectNodes(@"//Person");
+
+            foreach (XmlNode item in nodes)
+            {
+                XmlAttribute id = item.Attributes["id"];
+                XmlNode idNode = item.SelectSingleNode(@".//@id");
+                XmlNode vorname = item.SelectSingleNode(@".//Vorname");
+                XmlNode nachname = item.SelectSingleNode(@".//Nachname");
+                XmlNode alter = item.SelectSingleNode(@".//Alter");
+                XmlNodeList kinder = item.SelectNodes(@".//Kinder");
+
+                Console.WriteLine(id.InnerText + " " + vorname.InnerText + " " + nachname.InnerText + " " + alter.InnerText);
+                if (kinder.Count > 0)
+                    Console.WriteLine("\tKinder: ");
+                foreach (XmlNode kind in kinder)
+                {
+                    XmlNode name = kind.SelectSingleNode(@".//Name");
+                    Console.WriteLine("\t\t" + name.InnerText);
+                }
+            }
         }
     }
 }
