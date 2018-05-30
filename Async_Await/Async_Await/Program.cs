1137e09aa3ec6a045c823a58266038f42d6ec674
Kevin Hetzendorfer <hetzi.hetzendorfer@gmx.at>
2018-05-30 11:35:47 +0200

    asd

---
+++ Async_Await/Async_Await/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Async_Await
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task<string> task = Task<string>.Run(() =>
+            //{
+            //    System.Threading.Thread.Sleep(3000);
+            //    return DateTime.Now.ToLongDateString();
+            //});
+            //Console.WriteLine("Ich warte .....");
+            //Console.WriteLine("{0}", task.Result);
+            //Console.ReadLine();
+
+            Start();
+            Console.WriteLine("Ende Main ...");
+            Console.ReadLine();
+        }
+
+        static async void Start()
+        {
+            await DoSomethingAsync(1);
+            Console.WriteLine("Nach Aufruf von SoSomethingAsync(1) ...");
+        }
+
+        static void DoSomething(int id)
+        {
+            Console.WriteLine("Operation {0} startet ...", id);
+            System.Threading.Thread.Sleep(2000);
+            Console.WriteLine("Operation {0} ist beendet ...", id);
+        }
+
+        static Task DoSomethingAsync(int id)
+        {
+            Task task = Task.Run(() =>
+            {
+                Console.WriteLine("Operation {0} startet ...", id);
+                System.Threading.Thread.Sleep(2000);
+                Console.WriteLine("Operation {0} ist beendet ...", id);
+            });
+            return task;
+        }
+    }
+}
---
+++ Async_Await/Async_Await/Webserver.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace Async_Await
+{
+    class Webserver
+    {
+
+        public void MethodA(string s)
+        {
+
+        }
+
+        public void MEthodB() { }
+    }
+
+    class A
+    {
+        public A(string s) { }
+    }
+    class B
+    {
+        void C()
+        {
+            A a = new A("a");
+        }
+    }
+}
